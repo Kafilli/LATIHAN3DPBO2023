67d8787b764ca36da86b37b44ebd14ecc0b9f121
Kafilli <100756191+Kafilli@users.noreply.github.com>
2023-03-01 11:50:51 +0700

    Update prodi.py

--- PYTHON/prodi.py
+++ PYTHON/prodi.py
@@ -1,27 +1,33 @@
-#class prodi yang memiliki atau has a class mahasiswa, dosen, dan course
 class prodi:
     # atribut private
     __namaprodi = ""
+    __kodeprodi = ""
     __listmhs = []
     __listdosen = []
     __listcourse = []
 
     # constructor
-    def __init__(self, namaprodi):
+    def __init__(self, namaprodi, kodeprodi):
         self.__namaprodi = namaprodi
-    
-    #setter dan getter
+        self.__kodeprodi = kodeprodi
+
     def setnamaprodi(self, prodi):
         self.__namaprodi = prodi
 
     def getnamaprodi(self):
         return self.__namaprodi
 
+    def setkodeprodi(self, prodi):
+        self.__kodeprodi = prodi
+
+    def getkodeprodi(self):
+        return self.__kodeprodi
+
     def addmhs(self, mhs):
         self.__listmhs.append(mhs)
 
     def getlistmhs(self):
-        print(f"Mahasiswa pada prodi {self.__namaprodi} :")
+        print(f"Mahasiswa pada prodi {self.__namaprodi} {self.__kodeprodi}:")
         i = 1
         for mhs in self.__listmhs:
             print(f"{i}. {mhs.getnama()}")
@@ -31,7 +37,7 @@ class prodi:
         self.__listdosen.append(dosen)
 
     def getlistdosen(self):
-        print(f"Dosen pada prodi {self.__namaprodi} :")
+        print(f"Dosen pada prodi {self.__namaprodi} {self.__kodeprodi}:")
         i = 1
         for dosen in self.__listdosen:
             print(f"{i}. {dosen.getnama()}")
@@ -41,7 +47,7 @@ class prodi:
         self.__listcourse.append(course)
 
     def getlistcourse(self):
-        print(f"Matkul pada prodi {self.__namaprodi} :")
+        print(f"Matkul pada prodi {self.__namaprodi} {self.__namaprodi}:")
         i = 1
         for course in self.__listcourse:
             print(f"{i}. {course.getnamamatkul()}")
